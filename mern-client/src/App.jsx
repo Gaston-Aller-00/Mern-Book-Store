35481b36188e97e494db02e6807c62a2637058f6
Gaston <gastonallerdev@gmail.com>
2024-01-11 18:55:31 -0300

    first

---
+++ mern-client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='text-2xl bg-red-500'>MERN</div>
+    </>
+  )
+}
+
+export default App
