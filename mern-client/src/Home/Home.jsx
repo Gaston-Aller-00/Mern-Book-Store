43fff5ea54bf56918add53498c77b148689249c8
Gaston <gastonallerdev@gmail.com>
2024-01-15 16:41:11 -0300

    Home

--- mern-client/src/Home/Home.jsx
+++ mern-client/src/Home/Home.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Banner from "../components/Banner";
-import FavoriteBooks from "./FavoriteBooks";
-
+import BestSellerBooks from "./BestSellerBooks";
+import FavBook from "./FavBook";
+import PromoBanner from "./PromoBanner";
+import OtherBooks from "./OtherBooks";
+import Review from "./Review";
+// all shit
 const Home = () => {
   return (
     <div>
       <Banner />
-      <FavoriteBooks/>
+      <BestSellerBooks/>
+      <FavBook/>
+      <PromoBanner/>
+      <OtherBooks/>
+      <Review/>
+     
     </div>
   );
 };
