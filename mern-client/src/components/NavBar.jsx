a272f4f66d9a45f711ac424e54c0567bde99dc43
Gaston <gastonallerdev@gmail.com>
2024-01-16 02:53:56 -0300

    Navbar

--- mern-client/src/components/NavBar.jsx
+++ mern-client/src/components/NavBar.jsx
@@ -31,14 +31,14 @@ const NavBar = () => {
 
   const navItems = [
     { link: "Home", path: "/" },
-    { link: "About", path: "about" },
+    { link: "About", path: "/about" },
     { link: "Shop", path: "/shop" },
     { link: "Sell Your Book", path: "/admin/dashboard" },
     { link: "Blog", path: "/blog" },
   ];
 
   return (
-    <header className="w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300">
+    <header  className="w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300">
       <nav className={`py-4 lg:px-24 px-4 ${isSticky ? "sticky top-0 left-0 right-0 bg-blue-300 transition-all duration-300 " : ""} `}>
         <div className="flex justify-between items-center text-base gap-8">
           {/* logo */}
