fa38ce9a9e4110bdb0a8dba754961b20a99ba690
Gaston <gastonallerdev@gmail.com>
2024-01-17 01:40:00 -0300

    component

---
+++ mern-client/src/components/Blog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <div>Blog111asasassa</div>
+  )
+}
+
+export default Blog
--- mern-client/src/components/BooksCards.jsx
+++ mern-client/src/components/BooksCards.jsx
@@ -22,7 +22,7 @@ const BooksCards = ({ headline, books }) => {
       </h2>
       {/* cards */}
 
-      <div>
+      <div className="mt-12">
         <Swiper
           slidesPerView={1}
           spaceBetween={10}
