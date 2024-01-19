4d987b3f279a2f26b68c15a778c89b92f99a423e
Gaston <gastonallerdev@gmail.com>
2024-01-19 00:24:22 -0300

    shop&singleBook

--- mern-client/src/shop/Shop.jsx
+++ mern-client/src/shop/Shop.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Card } from "flowbite-react";
+import { Link, useLoaderData } from "react-router-dom";
 //cards en la seccion shop
 const Shop = () => {
   const [books, setBooks] = useState([]);
@@ -11,7 +12,8 @@ const Shop = () => {
   }, []);
 
   return (
-    <div className="mt-28 px-4 lg:px-24">
+    
+    <div className="cursor-pointer mt-28 px-4 lg:px-24">
       <h2 className="text-5xl font-bold text-center">All Books are here</h2>
   
       <div className="grid gap-8 my-12 lg:grid-cols-4 sm:grid-cols-2 md:grid-col-3 grid-cols-1">
@@ -21,11 +23,14 @@ const Shop = () => {
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {book.bookTitle}
             </h5>
-            {/* se oculta la descripcion luego de la 4ta linea */}
+            {/* hidden desc despues de la 4 linea */}
             <p className="font-normal text-gray-700 dark:text-gray-400 overflow-hidden" style={{ maxHeight: '6em', WebkitLineClamp: 4 }}>
               {book.bookDescription}
             </p>
-            <button className="btnBlue">Buy Now!</button>
+            {/* lead to book id */}
+            <Link to={`/book/${book._id}`}>
+            <button className="btnBlue w-full ">Buy Now!</button>
+            </Link>
           </Card>
         ))}
       </div>
--- mern-client/src/shop/SingleBook.jsx
+++ mern-client/src/shop/SingleBook.jsx
@@ -1,15 +1,67 @@
-import React from 'react'
-import { useLoaderData } from 'react-router-dom'
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { Card } from "flowbite-react";
+import { PiShareNetwork } from "react-icons/pi";
+
 //click en la card y se muestra la info del book
 const SingleBook = () => {
-    const {_id,imageURL,bookTitle} = useLoaderData();
-  return (
-    
-   <div className='mt-28 px-4 lg:px-24'>
-    <img src={imageURL} alt="coverBook" className='h-96' />
-    <h2>{bookTitle}</h2>
-   </div>
-  )
-}
+  const {
+    _id,
+    imageURL,
+    bookTitle,
+    authorName,
+    category,
+    bookDescription,
+    bookPdfUrl,
+  } = useLoaderData();
 
-export default SingleBook
+  const imageStyle = {
+    height: "430px",
+    width: "480px",
+    objectFit: "contain",
+    borderRadius: "16px",
+  };
+  return (
+    <div className="mt-40 flex justify-center items-center gap-4 ">
+      <img style={imageStyle} src={imageURL} alt="bookCover" />
+      <Card className="-ml-24 w-full lg:w-3/4 xl:w-1/2">
+        <div className="p-11">
+          <div className="flex items-center justify-between">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white mb-2">
+              {bookTitle}
+            </h2>
+
+            <a href={bookPdfUrl} target="_blank" rel="noopener noreferrer">
+              <PiShareNetwork className="text-3xl" />
+            </a>
+          </div>
+
+          <h3 className="text-xl font-semibold mb-4">{authorName}</h3>
+          <p className="text-black text-base mb-4">{category}</p>
+          <p className="text-gray-700 dark:text-gray-400">{bookDescription}</p>
+        </div>
+        <div className="flex items-center justify-center gap-4 ">
+          <span className="text-3xl font-bold text-gray-900 dark:text-white">
+            $10.00
+          </span>
+
+          <a
+            href="#"
+            className="rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+          >
+            Add to cart
+          </a>
+        </div>
+      </Card>
+    </div>
+  );
+};
+export default SingleBook;
+
+// <div className="mt-28 px-4 lg:px-24 flex justify-center items-center">
+//   <img src={imageURL} alt="coverBook" className="h-96" />
+//   <div>
+//     <h2>{bookTitle}</h2>
+//     <h2>{authorName}</h2>
+//   </div>
+// </div>
