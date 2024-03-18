2e634e774af383ad739814961ba6a890b0fe5f21
Gaston <gastonallerdev@gmail.com>
2024-03-18 20:24:17 -0300

    more UX

--- mern-client/src/Home/BestSellerBooks.jsx
+++ mern-client/src/Home/BestSellerBooks.jsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useFetchData from "../hooks/useFetchData";
 import BooksCards from "../components/BooksCards";
 import { PuffLoader } from "react-spinners";
 
 const BestSellerBooks = () => {
+ 
+
   const { data: books, isLoading } = useFetchData(
     "https://mern-book-store-eta.vercel.app/all-books",
     0,
     16
   );
 
+  const [isLoading2, setIsLoading2] = useState(true);
+  const [books2, setBooks] = useState(null);
+
+  useEffect(() => {
+    fetch("https://mern-book-store-eta.vercel.app/all-books")
+      .then((res) => res.json())
+      .then((data) => {
+        setBooks(data);
+        setIsLoading2(false);
+      });
+  }, []);
+
+  if (isLoading2)
+    return (
+      <p className=" flex items-center justify-center text-3xl font-extrabold mt-5 ">
+        If books load slowly, refresh the page.
+      </p>
+    );
   return (
     <div>
       {/* spinner */}
-      {isLoading && (
+      {isLoading2 && (
         <div
           style={{
             display: "flex",
@@ -32,7 +52,7 @@ const BestSellerBooks = () => {
 
       {/* books cuando  */}
       {!isLoading && (
-        <BooksCards books={books} headline="Best Seller " headline2="Books" />
+        <BooksCards books={books}  headline="Best Seller " headline2="Books" />
       )}
     </div>
   );
