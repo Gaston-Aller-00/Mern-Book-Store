f93947cf59e58c2a4110a93d0403a452cb6b05a3
Gaston <gastonallerdev@gmail.com>
2024-03-18 20:24:27 -0300

    if loadingg

--- mern-client/src/Home/OtherBooks.jsx
+++ mern-client/src/Home/OtherBooks.jsx
@@ -10,6 +10,26 @@ const OtherBooks = () => {
     16
   );
 
+
+  const [isLoading2, setIsLoading2] = useState(true);
+  const [books2, setBooks] = useState(null);
+
+  useEffect(() => {
+    fetch("https://mern-book-store-eta.vercel.app/all-books")
+      .then((res) => res.json())
+      .then((data) => {
+        setBooks(data);
+        setIsLoading2(false);
+      });
+  }, []);
+
+  if (isLoading2)
+    return (
+      <p className=" flex items-center justify-center text-3xl font-extrabold mt-5 ">
+        If books load slowly, refresh the page.
+      </p>
+    );
+
   return (
     <div>
       {isLoading && (
